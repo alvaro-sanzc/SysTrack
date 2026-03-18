1ab956838dd596b2aea605e990320b99c38f5e54
alvaro-sanzc <76051058+alvaro-sanzc@users.noreply.github.com>
2026-03-18 19:17:50 +0100

    setup express app

---
+++ backend/src/app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+
+const app = express();
+
+// Middlewares
+app.use(express.json());
+
+// Routes
+app.get('/', (req, res) => {
+  res.send('API funcionando');
+});
+
+// 404 handler
+app.use((req, res) => {
+  res.status(404).json({
+    error: 404,
+    message: 'Ruta no encontrada'
+  });
+});
+
+// Error handler
+app.use((err, req, res, next) => {
+  res.status(err.status || 500).json({
+    message: err.message || 'Error interno del servidor'
+  });
+});
+
+module.exports = app;
